9e5fa455d9f1c8f31f5e039334310e5b095bf7c7
kontura <matej.ales@seznam.cz>
2018-12-08 09:48:59 +0100

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p ~/.config 
+
 ln -sfv "$DOTFILES_DIR/.xinitrc" ~
 ln -sfv "$DOTFILES_DIR/.Xresources" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
@@ -11,3 +13,7 @@ ln -sfv "$DOTFILES_DIR/sxhkd" ~/.config/sxhkd
 ln -sfv "$DOTFILES_DIR/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
+
+mkdir -p ~/src/dwm
+cd ~/src/dwm && git clone https://git.suckless.org/dwm
+ln -sfv "$DOTFILES_DIR/dwm/config.h" ~/src/dwm/config.h
