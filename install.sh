def9b568d85562d739f951bff39b85e0442af17f
Aleš Matěj <amatej@redhat.com>
2019-11-24 21:53:32 +0100

    update minors

--- install.sh
+++ install.sh
@@ -10,17 +10,17 @@ ln -sfv "$DOTFILES_DIR/.xinitrc" ~
 ln -sfv "$DOTFILES_DIR/.Xresources" ~
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/sxhkd" ~/.config/sxhkd
-ln -sfv "$DOTFILES_DIR/tig" ~/.config/tig
-ln -sfv "$DOTFILES_DIR/waybar" ~/.config/waybar
-ln -sfv "$DOTFILES_DIR/mako" ~/.config/mako
+ln -sfv "$DOTFILES_DIR/sxhkd" ~/.config/
+ln -sfv "$DOTFILES_DIR/tig" ~/.config/
+ln -sfv "$DOTFILES_DIR/waybar" ~/.config/
+ln -sfv "$DOTFILES_DIR/mako" ~/.config/
 ln -sfv "$DOTFILES_DIR/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 
-ln -sfv "$DOTFILES_DIR/wayfire.ini" ~/.config/wayfire.ini
-ln -sfv "$DOTFILES_DIR/wf-shell.ini" ~/.config/wf-shell.ini
+ln -sfv "$DOTFILES_DIR/wayfire.ini" ~/.config/
+ln -sfv "$DOTFILES_DIR/wf-shell.ini" ~/.config/
 
 mkdir -p ~/src
 cd ~/src && git clone https://git.suckless.org/dwm
